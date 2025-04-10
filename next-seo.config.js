6f57f3550d7ff9cb0bb897353c181b4204638169
lucasadilla <lucas.pentlandhyde@gmail.com>
2025-04-10 11:22:17 -0400

    seo

---
+++ next-seo.config.js
@@ -0,0 +1,11 @@
+export default {
+  title: 'Bicker',
+  description: 'the game of instigating, debating, and deliberating!',
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://www.yoursite.com/',
+    site_name: 'Bicker',
+  },
+
+}; 
