f526299855028eefe34eb806931f4837911a9fe9
lucasadilla <lucas.pentlandhyde@gmail.com>
2025-04-16 11:56:52 -0400

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { SessionProvider } from 'next-auth/react';
 import NavBar from '../components/NavBar';
 import { DefaultSeo } from 'next-seo';
 import SEO from '../next-seo.config';
+import { Analytics } from "@vercel/analytics/react"
 
 
 export default function MyApp({ Component, pageProps: { session, ...pageProps } }) {
@@ -11,6 +12,7 @@ export default function MyApp({ Component, pageProps: { session, ...pageProps } 
             <DefaultSeo {...SEO} />
             <NavBar />
             <Component {...pageProps} />
+            <Analytics/>
         </SessionProvider>
     );
 }
