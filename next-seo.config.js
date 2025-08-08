471c84b24174a5f0f2cc5dc82a5a2d6cf3bc3c82
Lucas Pentland-Hyde <58784755+lucasadilla@users.noreply.github.com>
2025-08-07 20:08:53 -0400

    Improve SEO metadata and sitemap

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,18 +1,32 @@
 export default {
   title: 'Bicker',
   description: 'The game of instigating, debating, and deliberating!',
+  canonical: 'https://bicker.ca/',
   additionalMetaTags: [
     {
       name: 'keywords',
       content:
         'bicker, debate, deliberate, instigate, online debate game',
     },
+    { name: 'robots', content: 'index, follow' },
   ],
   openGraph: {
     type: 'website',
     locale: 'en_US',
     url: 'https://bicker.ca/',
     site_name: 'Bicker',
+    images: [
+      {
+        url: 'https://bicker.ca/favicon_io/android-chrome-512x512.png',
+        width: 512,
+        height: 512,
+        alt: 'Bicker Logo',
+      },
+    ],
+  },
+
+  twitter: {
+    cardType: 'summary_large_image',
   },
 
 };
